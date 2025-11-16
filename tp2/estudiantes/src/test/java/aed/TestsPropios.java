38ad0e066a1bfc206389eb706176bf2942c2ad77
totodm89 <tomasdominguezmues89@gmail.com>
2025-11-15 21:42:58 -0300

    test_propias_agregados

--- tp2/estudiantes/src/test/java/aed/TestsPropios.java
+++ tp2/estudiantes/src/test/java/aed/TestsPropios.java
@@ -3,6 +3,10 @@ package aed;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.Arrays;
 
 public class TestsPropios {
@@ -16,7 +20,7 @@ public class TestsPropios {
 
    @BeforeEach
    void setUp() {
-      this.d_aula = 5;
+      this.d_aula = 7;
       this.cant_alumnos = 4;
       this.solucion = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
       this.edr = new Edr(this.d_aula, this.cant_alumnos, this.solucion);
@@ -77,17 +81,120 @@ public class TestsPropios {
       Assertions.assertTrue(Arrays.equals(notas_esperadas, notas));
    }
 
-   @Test
-   void todos_misma_nota() {
+
+@Test
+void test_Copiones_Solo_Del_De_Adelante() {
+    int dAula = 5; 
+    int cantAlumnos = 4;
+    int[] solucion = {1,2,3,4}; 
+    Edr edr = new Edr(dAula, cantAlumnos, solucion);
+
+    edr.resolver(0, 0, 1);
+    edr.resolver(0, 1, 1);
+    edr.resolver(0, 2, 1);
+    edr.resolver(0, 3, 1);
+
+    
+    edr.copiarse(1);
+    edr.copiarse(2);
+    edr.copiarse(3);
+
+    int[] copiones = edr.chequearCopias();
+
+    int[] esperados = {1, 2, 3};
+
+    assertTrue(Arrays.equals(esperados, copiones));
+}
+@Test
+   void alumno_se_copia_adelante() {
+      this.d_aula = 5;
+      this.cant_alumnos = 8;
+      this.solucion = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+      this.edr = new Edr(this.d_aula, this.cant_alumnos, this.solucion);
       this.edr.resolver(0, 0, 0);
       this.edr.resolver(1, 1, 1);
       this.edr.resolver(2, 2, 2);
       this.edr.resolver(3, 3, 3);
-      this.edr.entregar(0);
-      this.edr.entregar(1);
-      this.edr.entregar(2);
-      this.edr.entregar(3);
-      NotaFinal[] notas = this.edr.corregir();
-      double[] var10000 = new double[]{10.0, 10.0, 10.0, 10.0};
+      this.edr.resolver(4, 4, 4);
+      this.edr.resolver(4, 5, 5);
+      this.edr.resolver(5, 5, 5);
+      this.edr.resolver(6, 6, 6);
+      this.edr.resolver(7, 7, 7);
+      this.edr.copiarse(7);
+      double[] notas = this.edr.notas();
+      double[] notas_esperadas = new double[]{10.0, 10.0, 10.0, 10.0, 20.0, 10.0, 10.0, 20.0};
+      Assertions.assertTrue(Arrays.equals(notas_esperadas, notas));
    }
+
+   @Test
+void alumnos_sin_adelante_no_pueden_copiar() {
+    this.d_aula = 3;
+    this.cant_alumnos = 3;
+    this.solucion = new int[]{0,1,2,3,4,5,6,7,8,9};
+    this.edr = new Edr(this.d_aula, this.cant_alumnos, this.solucion);
+
+    for(int i = 0; i < 3; i++){
+        this.edr.resolver(i, i, i);
+    }
+
+    this.edr.copiarse(1);
+    this.edr.copiarse(2);
+
+    double[] notas = this.edr.notas();
+    double[] notas_esperadas = new double[]{10.0, 20.0, 20.0};
+    
+ 
+    Assertions.assertTrue(Arrays.equals(notas_esperadas, notas));
+}
+@Test
+void todos_se_copian_del_que_adelante() {
+    this.d_aula = 4;
+    this.cant_alumnos = 4;
+    this.solucion = new int[]{0,1,2,3,4,5,6,7,8,9};
+    this.edr = new Edr(this.d_aula, this.cant_alumnos, this.solucion);
+
+    
+    this.edr.resolver(0, 0, 0);
+    
+    this.edr.copiarse(1);
+    this.edr.copiarse(2);
+    this.edr.copiarse(3);
+
+    double[] notas = this.edr.notas();
+    double[] notas_esperadas = new double[]{10.0, 10.0, 10.0, 10.0};
+    
+    Assertions.assertTrue(Arrays.equals(notas_esperadas, notas));
+}
+
+
+@Test
+void copias_por_columnas_adelante() {
+   
+    this.d_aula = 5;
+    this.cant_alumnos = 6;
+    this.solucion = new int[]{0,1,2,3,4,5,6,7,8,9};
+    this.edr = new Edr(this.d_aula, this.cant_alumnos, this.solucion);
+
+   
+    this.edr.resolver(0, 0, 0); 
+    this.edr.resolver(1, 1, 1); 
+    this.edr.resolver(2, 2, 2); 
+    this.edr.resolver(3, 3, 3); 
+    this.edr.resolver(4, 4, 4); 
+    this.edr.resolver(5, 5, 5); 
+
+    // solo se van a poder copiar del que esta adelante en la misma columna
+    this.edr.copiarse(2);
+    this.edr.copiarse(3); 
+    this.edr.copiarse(4); 
+    this.edr.copiarse(5); 
+
+    double[] notas = this.edr.notas();
+    
+    double[] notas_esperadas = new double[]{10.0, 10.0, 20.0, 20.0, 20.0, 20.0};
+    Assertions.assertTrue(Arrays.equals(notas_esperadas, notas));
+
+}
+
+
 }
